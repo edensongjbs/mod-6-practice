26047c97385fd7f10ea929ef1dc674e43c05d2b0
edensongjbs <edensongjbs@gmail.com>
2020-09-17 14:39:53 -0400

    add question 2

--- four-questions.js
+++ four-questions.js
@@ -1,4 +1,6 @@
+// https://medium.com/javascript-in-plain-english/i-use-these-4-questions-to-find-outstanding-javascript-developers-4a468ea17155
 
+// 2
 //Implementation of Array map function
 Array.prototype._map = function(callbackfn, thisArg) {
     let archiveArray = Array.prototype.slice.call(this)
@@ -26,3 +28,43 @@ Array.prototype._filter = function(callbackfn, thisArg) {
 
     return result
 }
+
+//2
+// option # 1
+
+let obj = {
+    _initValue: 0,
+    get a() {
+      this._initValue++;
+      return this._initValue
+    }
+  }
+  console.log(obj.a, obj.a, obj.a)
+
+  // option # 2, using Object.defineProperty
+
+let obj = {}
+Object.defineProperty(obj, 'a', {
+  get: (function(){
+    let initValue = 0;
+    return function(){
+      initValue++;
+      return initValue
+    }
+  })()
+})
+console.log(obj.a, obj.a, obj.a)
+
+// option # 3, with proxies
+
+let initValue = 0;
+let obj = new Proxy({}, {
+  get: function(item, property, itemProxy){
+    if(property === 'a'){
+      initValue++;
+      return initValue
+    }
+    return item[property]
+  }
+})
+console.log(obj.a, obj.a, obj.a)
